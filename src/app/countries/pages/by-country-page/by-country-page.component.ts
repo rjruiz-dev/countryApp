505c3f8b380122f2ddfb91be39ebf3f134e0d0b3
rjruiz <rjruizsf@gmail.com>
2024-10-13 18:31:36 -0300

    LocalStorage del store

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
@@ -7,13 +7,19 @@ import { Country } from '../../interfaces/country';
   templateUrl: './by-country-page.component.html',
   styles: ``
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit {
 
   public countries: Country[] = [];
+  public initialValue: string = '';
   
   constructor(
     private countriesServices: CountriesService
   ){}
+
+  ngOnInit(): void {
+    this.countries = this.countriesServices.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesServices.cacheStore.byCountries.term;
+  }
 
   searchByCountry( term: string): void {
     this.countriesServices.searchCountry( term )
--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 import { Region } from '../../interfaces/region.type';
@@ -8,15 +8,21 @@ import { Region } from '../../interfaces/region.type';
   templateUrl: './by-region-page.component.html',
   styles: ``
 })
-export class ByRegionPageComponent {
+export class ByRegionPageComponent implements OnInit {
 
   public countries: Country[] = [];
   public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
   public selectedRegion?: Region;
+  public initialValue: string = '';
    
   constructor(
     private countriesServices: CountriesService
   ){}
+  
+  ngOnInit(): void {
+    this.countries = this.countriesServices.cacheStore.byRegion.countries;
+    this.selectedRegion = this.countriesServices.cacheStore.byRegion.region;
+  }
   
   searchByRegion( region: Region): void {
     this.selectedRegion = region;
